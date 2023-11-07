724d73025d4c6fd15c5e132f18b52e747bc0d85a
Ryan <144127658+Ryan182005@users.noreply.github.com>
2023-11-07 10:05:36 -0300

    Add files via upload

---
+++ ex008.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(){
+
+int i;
+
+printf("Digite o numero desejado:");
+scanf("%d", &i);
+
+for (i = i; i >= 0; i--)
+{
+  printf("%d \n", i);
+}
+
+
+
+
+return 0;
+}
+
